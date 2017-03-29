94a81660ab1d6d1aae03b3a8fa5c30df615e9c78
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-03-29 14:47:37 +0200

    add files to repository

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'sass/style.sass'
+        }
+      }
+    },
+
+    watch: {
+      files: 'sass/*.sass',
+      tasks: ['sass']
+    },
+
+    browserSync: {
+        bsFiles: {
+            src : 'css/*.css'
+        },
+        options: {
+            watchTask: true,
+            server: {
+                baseDir: './'
+            }
+        }
+    }
+  });
+
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  // Default task(s).
+  grunt.registerTask('default', ['browserSync', 'watch']);
+};
