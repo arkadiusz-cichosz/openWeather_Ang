95d8cac6251eb1e846c8f4e19f1380bff3e67f93
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-03-30 14:06:11 +0200

    add new code

--- js/setLocation.js
+++ js/setLocation.js
@@ -1,6 +1,11 @@
+let myID = '77d4971436d26f540fd4b63a95bfb825';
+
 function setLocation () {
 	$('.location').hide(400);
 	//$('.location').css('display', 'none');
+	let cityName = $('#city').val();
+	let countryCode = $('#countryCode').val();
+	console.log('User set: ' + cityName + ' ' + countryCode);
 	console.log('Edit box is hidden');
 	return false;
 }
